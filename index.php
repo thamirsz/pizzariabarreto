99e92acabdf5b704ada9240c4d6b4b17d95913e2
Thamires Amaral Barreto <thamires.barreto@HINOVA.net>
2025-03-02 15:26:23 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizzaria Barreto - Pedidos</title>
+    <!--Bootstrap -->
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <!-- App CSS -->
+      <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <h1> Testando CSS<h1>
+</body>
+</html>
