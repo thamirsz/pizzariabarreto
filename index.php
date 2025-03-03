19d8d9e0c01ff4f33dee02fb39b605acee277fb3
Thamires Amaral Barreto <thamires.barreto@HINOVA.net>
2025-03-03 20:33:44 -0300

    html e css da página inicial

--- index.php
+++ index.php
@@ -1,16 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pizzaria Barreto - Pedidos</title>
-    <!--Bootstrap -->
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-     <!-- App CSS -->
-      <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
-    <h1> Testando CSS<h1>
-</body>
-</html>
+
+
+<?php 
+            include_once("templates/header.php");
+        ?>
+        <div id="main-banner">
+
+  </div>
+  <div id="main-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h2>Monte a pizza como desejar:</h2>
+          <form action="process/pizza.php" method="POST" id="pizza-form">
+            <div class="form-group">
+              <label for="borda">Borda:</label>
+              <select name="borda" id="borda" class="form-control">
+                <option value="">Selecione a borda</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="massa">Massa:</label>
+              <select name="massa" id="massa" class="form-control">
+                <option value="">Selecione a massa</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="sabores">Sabores: (Máximo 3)</label>
+              <select multiple name="sabores[]" id="sabores" class="form-control">
+              </select>
+            </div>
+            <div class="form-group">
+              <input type="submit" class="btn btn-primary" value="Fazer Pedido!">
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+       <?php
+        include_once("templates/footer.php");
+        ?>
+
+    
---
+++ templates/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizzaria Barreto</title>
+    <!--Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <!-- App CSS -->
+      <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+   <header>
+    <nav class="navbar navbar-expand-lg">
+        <!-- Logo da Pizzaria -->
+        <a href="index.php" class="navbar-brand">
+            <img src="img/logo_pizzariabarreto.png" alt="Pizzaria Barreto" id="logomarca">
+        </a>
+        <!-- Itens do menu -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a href="index.php" class="nav-link">Faça seu pedido</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<div class= "alert alert-success">
+    <p>Pedido feito com sucesso</p>
+
+</div>
